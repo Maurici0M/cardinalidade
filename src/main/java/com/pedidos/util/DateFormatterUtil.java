62107fc529af7f6600252c2337972cf29c4ea938
Maurici0M <mauriciomarquescosta7@gmail.com>
2025-04-13 18:32:36 -0300

    utility class added to allow formatting of date type fields

---
+++ src/main/java/com/pedidos/util/DateFormatterUtil.java
@@ -0,0 +1,31 @@
+package com.pedidos.util;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Objects;
+
+public class DateFormatterUtil {
+
+    public static String formatDataDDMMYYYY(LocalDate date){
+        //formata a data no padrão brasileiro 13/04/2025
+        if (Objects.nonNull(date)){
+            DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+            return date.format(dateTimeFormatter);
+        }
+
+        return null;
+    }
+
+    public static String formatDataYYYYMMDD(LocalDate date){
+        //formata a data no padrão brasileiro 13/04/2025
+        if (Objects.nonNull(date)){
+            DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy/MM/dd");
+
+            return date.format(dateTimeFormatter);
+        }
+
+        return null;
+    }
+
+}
