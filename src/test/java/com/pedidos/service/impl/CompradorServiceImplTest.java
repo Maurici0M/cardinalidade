73586362f4f693da4248488e0a44a58690c75eba
Maurici0M <mauriciomarquescosta7@gmail.com>
2025-04-14 18:31:42 -0300

    New scenarios and improvements in unit tests added

--- src/test/java/com/pedidos/service/impl/CompradorServiceImplTest.java
+++ src/test/java/com/pedidos/service/impl/CompradorServiceImplTest.java
@@ -40,6 +40,25 @@ class CompradorServiceImplTest {
     class registerBuyer{
 
         @Test
+        @DisplayName("If there are no validation reports in the registration, it must be carried out successfully.")
+        void BuyerRegisteredSuccessfully() {
+            //ARRANGE
+            var buyer = CompradorFactory.buyerWithCompleteData();
+
+            //ACT
+            BDDMockito.given(compradorRepository.findByCpf(buyer.getCpf())).willReturn(Optional.empty());
+            BDDMockito.given(compradorRepository.save(buyer)).willReturn(buyer);
+
+            //ASSERT
+            Assertions.assertDoesNotThrow( ()-> compradorService.registerBuyer(buyer));
+
+            //garante que HOUVE apenas uma interação do repository com os metodos
+            verify(compradorValidator, times(1)).validateAllDataRegistration(buyer);
+            verify(compradorRepository, times(1)).findByCpf(buyer.getCpf());
+            verify(compradorRepository, times(1)).save(buyer);
+        }
+
+        @Test
         @DisplayName("If the buyer's CPF is empty or null, an error should be displayed.")
         void buyerWithoutCPF() {
             //ARRANGE
@@ -51,7 +70,9 @@ class CompradorServiceImplTest {
             );
 
             //ASSERT
-            Assertions.assertThrows(ResponseStatusException.class, ()-> compradorService.registerBuyer(buyer));
+            ResponseStatusException exception = Assertions.assertThrows(
+                    ResponseStatusException.class, ()-> compradorService.registerBuyer(buyer)
+            );
 
             //garante que HOUVE apenas uma interação do metodo de validação
             verify(compradorValidator, times(1)).validateAllDataRegistration(buyer);
@@ -60,6 +81,8 @@ class CompradorServiceImplTest {
             verify(compradorRepository, never()).findById(buyer.getId());
             verify(compradorRepository, never()).findByCpf(buyer.getCpf());
             verify(compradorRepository, never()).save(buyer);
+
+            Assertions.assertEquals("O campo CPF é obrigatório e não pode estar vazio!", exception.getReason());
         }
 
         @Test
@@ -74,7 +97,9 @@ class CompradorServiceImplTest {
             );
 
             //ASSERT
-            Assertions.assertThrows(ResponseStatusException.class, ()-> compradorService.registerBuyer(buyer));
+            ResponseStatusException exception = Assertions.assertThrows(
+                    ResponseStatusException.class, ()-> compradorService.registerBuyer(buyer)
+            );
 
             //garante que HOUVE apenas uma interação do metodo de validação
             verify(compradorValidator, times(1)).validateAllDataRegistration(buyer);
@@ -83,6 +108,8 @@ class CompradorServiceImplTest {
             verify(compradorRepository, never()).findById(buyer.getId());
             verify(compradorRepository, never()).findByCpf(buyer.getCpf());
             verify(compradorRepository, never()).save(buyer);
+
+            Assertions.assertEquals("O campo NOME é obrigatório e não pode estar vazio!", exception.getReason());
         }
 
         @Test
@@ -97,7 +124,9 @@ class CompradorServiceImplTest {
             );
 
             //ASSERT
-            Assertions.assertThrows(ResponseStatusException.class, ()-> compradorService.registerBuyer(buyer));
+            ResponseStatusException exception = Assertions.assertThrows(
+                    ResponseStatusException.class, ()-> compradorService.registerBuyer(buyer)
+            );
 
             //garante que HOUVE apenas uma interação do metodo de validação
             verify(compradorValidator, times(1)).validateAllDataRegistration(buyer);
@@ -106,6 +135,8 @@ class CompradorServiceImplTest {
             verify(compradorRepository, never()).findById(buyer.getId());
             verify(compradorRepository, never()).findByCpf(buyer.getCpf());
             verify(compradorRepository, never()).save(buyer);
+
+            Assertions.assertEquals("O campo SOBRENOME é obrigatório e não pode estar vazio!", exception.getReason());
         }
 
         @Test
@@ -120,7 +151,9 @@ class CompradorServiceImplTest {
             );
 
             //ASSERT
-            Assertions.assertThrows(ResponseStatusException.class, ()-> compradorService.registerBuyer(buyer));
+            ResponseStatusException exception = Assertions.assertThrows(
+                    ResponseStatusException.class, ()-> compradorService.registerBuyer(buyer)
+            );
 
             //garante que HOUVE apenas uma interação do metodo de validação
             verify(compradorValidator, times(1)).validateAllDataRegistration(buyer);
@@ -129,6 +162,8 @@ class CompradorServiceImplTest {
             verify(compradorRepository, never()).findById(buyer.getId());
             verify(compradorRepository, never()).findByCpf(buyer.getCpf());
             verify(compradorRepository, never()).save(buyer);
+
+            Assertions.assertEquals("O campo data de nascimento é de preenchimento obrigatório e não pode estar vazio!", exception.getReason());
         }
 
         @Test
@@ -143,7 +178,9 @@ class CompradorServiceImplTest {
             );
 
             //ASSERT
-            Assertions.assertThrows(ResponseStatusException.class, ()-> compradorService.registerBuyer(buyer));
+            ResponseStatusException exception = Assertions.assertThrows(
+                    ResponseStatusException.class, ()-> compradorService.registerBuyer(buyer)
+            );
 
             //garante que HOUVE apenas uma interação do metodo de validação
             verify(compradorValidator, times(1)).validateAllDataRegistration(buyer);
@@ -152,6 +189,182 @@ class CompradorServiceImplTest {
             verify(compradorRepository, never()).findById(buyer.getId());
             verify(compradorRepository, never()).findByCpf(buyer.getCpf());
             verify(compradorRepository, never()).save(buyer);
+
+            Assertions.assertEquals("É necessário ter no mínimo 18 anos para se cadastrar na plataforma!", exception.getReason());
+        }
+
+        @Test
+        @DisplayName("If the address's zip code is empty or null, an error should be displayed.")
+        void buyerWithoutZipCode() {
+            //ARRANGE
+            var buyer = CompradorFactory.buyerInsertAderessData(
+                    "        ",
+                    "Praça da Sé",
+                    "Sé",
+                    "4892",
+                    "Casa",
+                    "sp"
+            );
+
+            //ASSERT
+            ResponseStatusException exception = Assertions.assertThrows(
+                    ResponseStatusException.class, ()-> compradorService.registerBuyer(buyer)
+            );
+
+            //garante que HOUVE apenas uma interação do metodo de validação
+            verify(compradorValidator, times(1)).validateAllDataRegistration(buyer);
+
+            //garante que NÃO HOUVE interação do repository com os seguintes metodos
+            verify(compradorRepository, never()).findById(buyer.getId());
+            verify(compradorRepository, never()).findByCpf(buyer.getCpf());
+            verify(compradorRepository, never()).save(buyer);
+
+            Assertions.assertEquals("O campo CEP é obrigatório e não pode estar vazio!", exception.getReason());
+        }
+
+        @Test
+        @DisplayName("If the address's street is empty or null, an error should be reported.")
+        void buyerWithoutAderessStreet() {
+            //ARRANGE
+            var buyer = CompradorFactory.buyerInsertAderessData(
+                    "01001-000",
+                    null,
+                    "Sé",
+                    "4892",
+                    "Casa",
+                    "sp"
+            );
+
+            //ASSERT
+            ResponseStatusException exception = Assertions.assertThrows(
+                    ResponseStatusException.class, ()-> compradorService.registerBuyer(buyer)
+            );
+
+            //garante que HOUVE apenas uma interação do metodo de validação
+            verify(compradorValidator, times(1)).validateAllDataRegistration(buyer);
+
+            //garante que NÃO HOUVE interação do repository com os seguintes metodos
+            verify(compradorRepository, never()).findById(buyer.getId());
+            verify(compradorRepository, never()).findByCpf(buyer.getCpf());
+            verify(compradorRepository, never()).save(buyer);
+
+            Assertions.assertEquals("O campo LOGRADOURO é obrigatório e não pode estar vazio!", exception.getReason());
+        }
+
+        @Test
+        @DisplayName("If the address neighborhood is empty or null, an error should be reported.")
+        void buyerWithoutAderessNeighborhood() {
+            //ARRANGE
+            var buyer = CompradorFactory.buyerInsertAderessData(
+                    "01001-000",
+                    "Praça da Sé",
+                    null,
+                    "4892",
+                    "Casa",
+                    "sp"
+            );
+
+            //ASSERT
+            ResponseStatusException exception = Assertions.assertThrows(
+                    ResponseStatusException.class, ()-> compradorService.registerBuyer(buyer)
+            );
+
+            //garante que HOUVE apenas uma interação do metodo de validação
+            verify(compradorValidator, times(1)).validateAllDataRegistration(buyer);
+
+            //garante que NÃO HOUVE interação do repository com os seguintes metodos
+            verify(compradorRepository, never()).findById(buyer.getId());
+            verify(compradorRepository, never()).findByCpf(buyer.getCpf());
+            verify(compradorRepository, never()).save(buyer);
+
+            Assertions.assertEquals("O campo BAIRRO é obrigatório e não pode estar vazio!", exception.getReason());
+        }
+
+        @Test
+        @DisplayName("If the address number is empty or null, an error should be reported.")
+        void buyerWithoutAderessNumber() {
+            //ARRANGE
+            var buyer = CompradorFactory.buyerInsertAderessData(
+                    "01001-000",
+                    "Praça da Sé",
+                    "Sé",
+                    "              ",
+                    "Casa",
+                    "sp"
+            );
+
+            //ASSERT
+            ResponseStatusException exception = Assertions.assertThrows(
+                    ResponseStatusException.class, ()-> compradorService.registerBuyer(buyer)
+            );
+
+            //garante que HOUVE apenas uma interação do metodo de validação
+            verify(compradorValidator, times(1)).validateAllDataRegistration(buyer);
+
+            //garante que NÃO HOUVE interação do repository com os seguintes metodos
+            verify(compradorRepository, never()).findById(buyer.getId());
+            verify(compradorRepository, never()).findByCpf(buyer.getCpf());
+            verify(compradorRepository, never()).save(buyer);
+
+            Assertions.assertEquals("O campo NÚMERO é obrigatório e não pode estar vazio!", exception.getReason());
+        }
+
+        @Test
+        @DisplayName("If the address complement is empty or null, an error should be reported.")
+        void buyerWithoutAderessComplement() {
+            //ARRANGE
+            var buyer = CompradorFactory.buyerInsertAderessData(
+                    "01001-000",
+                    "Praça da Sé",
+                    "Sé",
+                    "4892",
+                    null,
+                    "sp"
+            );
+
+            //ASSERT
+            ResponseStatusException exception = Assertions.assertThrows(
+                    ResponseStatusException.class, ()-> compradorService.registerBuyer(buyer)
+            );
+
+            //garante que HOUVE apenas uma interação do metodo de validação
+            verify(compradorValidator, times(1)).validateAllDataRegistration(buyer);
+
+            //garante que NÃO HOUVE interação do repository com os seguintes metodos
+            verify(compradorRepository, never()).findById(buyer.getId());
+            verify(compradorRepository, never()).findByCpf(buyer.getCpf());
+            verify(compradorRepository, never()).save(buyer);
+
+            Assertions.assertEquals("O campo COMPLEMENTO é obrigatório e não pode estar vazio!", exception.getReason());
+        }
+
+        @Test
+        @DisplayName("If the address's UF is empty or null, an error should be reported.")
+        void buyerWithoutAderessUF() {
+            //ARRANGE
+            var buyer = CompradorFactory.buyerInsertAderessData(
+                    "01001-000",
+                    "Praça da Sé",
+                    "Sé",
+                    "4892",
+                    "CS",
+                    "               "
+            );
+
+            //ASSERT
+            ResponseStatusException exception = Assertions.assertThrows(
+                    ResponseStatusException.class, ()-> compradorService.registerBuyer(buyer)
+            );
+
+            //garante que HOUVE apenas uma interação do metodo de validação
+            verify(compradorValidator, times(1)).validateAllDataRegistration(buyer);
+
+            //garante que NÃO HOUVE interação do repository com os seguintes metodos
+            verify(compradorRepository, never()).findById(buyer.getId());
+            verify(compradorRepository, never()).findByCpf(buyer.getCpf());
+            verify(compradorRepository, never()).save(buyer);
+
+            Assertions.assertEquals("O campo UF é obrigatório e não pode estar vazio!", exception.getReason());
         }
 
         @Test
@@ -164,35 +377,21 @@ class CompradorServiceImplTest {
             BDDMockito.given(compradorRepository.findByCpf(buyer.getCpf())).willReturn(Optional.of(buyer));
 
             //ASSERT
-            Assertions.assertThrows(ResponseStatusException.class, ()-> compradorService.registerBuyer(buyer));
+            ResponseStatusException exception = Assertions.assertThrows(
+                    ResponseStatusException.class, ()-> compradorService.registerBuyer(buyer)
+            );
 
             //garante que HOUVE apenas uma interação do repository com o metodo findById
             verify(compradorRepository, times(1)).findByCpf(buyer.getCpf());
 
             //garante que NÃO HOUVE interação do repository com os seguintes metodos
             verify(compradorRepository, never()).save(buyer);
-        }
 
-        @Test
-        @DisplayName("If the CPF search returns a buyer, it should report an error")
-        void BuyerRegisteredSuccessfully() {
-            //ARRANGE
-            var buyer = CompradorFactory.buyerWithCompleteData();
-
-            //ACT
-            BDDMockito.given(compradorRepository.findByCpf(buyer.getCpf())).willReturn(Optional.of(buyer));
-
-            //ASSERT
-            Assertions.assertThrows(ResponseStatusException.class ,()-> compradorService.registerBuyer(buyer));
-
-            //garante que HOUVE apenas uma interação do repository com o metodo findById
-            verify(compradorValidator, times(1)).validateAllDataRegistration(buyer);
-            verify(compradorRepository, times(1)).findByCpf(buyer.getCpf());
-
-            //garante que o metodo save nao foi invocado durante o teste
-            verify(compradorRepository, never()).save(buyer);
+            Assertions.assertEquals("O comprador já está cadastrado. Somente é permitido um cadastro por CPF!", exception.getReason());
         }
 
     }
 
 }
+
+
